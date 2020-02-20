dd188876d32d9e053d9fcb758f7df23848be3e97
Jonathan Dick <jdick@ampath.or.ke>
2020-02-20 21:44:32 +0300

    self-review

--- src/programs/programs-widget.tsx
+++ src/programs/programs-widget.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ProgramsBriefSummary from "./programs-brief-summary.component";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import {backendDependencies} from "openmrs-esm-patient-chart-widgets";
 
 
 export function ProgramsWidget(props) {
+    console.log(backendDependencies);
     return (
         <BrowserRouter basename={window["getOpenmrsSpaBase"]()}>
             <ProgramsBriefSummary/>
+            
             <Link to="hello/world">Hello</Link>
             <Route exact path="/patient/:patientUuid/chart/hello/world">
                 <div>Hello world!</div> 
